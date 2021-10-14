4054a286d593181f15bae0f281b4ecbebb55a962
prashant169 <prashantphad2018@gmail.com>
2021-10-14 01:54:20 -0800

    Program check last name is valid or not

---
+++ UserRegistrationRegex/src/com/bridgelabz/regex/Regex.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.regex;
+
+public class Regex {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the User Registration Problem using Regex!!");
+        UserOperator user = new UserOperator();
+        user.firstName("Rajendra");
+        user.lastName("Gund");
+    }
+}
