d3c209b983dcf5b481c576cb360b9749a61499c4
prashant169 <prashantphad2018@gmail.com>
2021-10-14 08:32:34 -0800

    Added: Check email address is valid or not

--- UserRegistrationRegex/src/com/bridgelabz/regex/Regex.java
+++ UserRegistrationRegex/src/com/bridgelabz/regex/Regex.java
@@ -1,10 +1,11 @@
 package com.bridgelabz.regex;
 
 public class Regex {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the User Registration Problem using Regex!!");
-        UserOperator user = new UserOperator();
-        user.firstName("Rajendra");
-        user.lastName("Gund");
-    }
+	public static void main(String[] args) {
+		System.out.println("Welcome to the User Registration Problem using Regex!!");
+		UserOperator user = new UserOperator();
+		user.firstName("prashant");
+		user.lastName("phad");
+		user.emailaddress("prashantphad2018@gmail.com");
+	}
 }
