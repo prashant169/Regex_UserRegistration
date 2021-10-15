b20930402db56e78ab811ffef7701f817f440a8e
prashant169 <prashantphad2018@gmail.com>
2021-10-14 21:41:37 -0800

    Added: Rule 2 Should have at least 1 upper case

--- UserRegistrationRegex/src/com/bridgelabz/regex/Regex.java
+++ UserRegistrationRegex/src/com/bridgelabz/regex/Regex.java
@@ -1,13 +1,13 @@
 package com.bridgelabz.regex;
 
 public class Regex {
-	public static void main(String[] args) {
-		System.out.println("Welcome to the User Registration Problem using Regex!!");
-		UserOperator user = new UserOperator();
-		user.firstName("Prashant");
-		user.lastName("Phad");
-		user.emailaddress("prashantphad2018@gmail.com");
-		user.phoneNo("91 9970899057");
-		user.passRule1("123asASD");
-	}
+    public static void main(String[] args) {
+        System.out.println("Welcome to the User Registration Problem using Regex!!");
+        UserOperator user = new UserOperator();
+        user.firstName("Prashant");
+        user.lastName("Phad");
+        user.emailaddress("prashantphad2018@gmail.com");
+        user.phoneNo("91 9970899057");
+        user.passward("Phad1234");
+    }
 }
