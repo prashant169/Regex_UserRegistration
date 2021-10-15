86884af86d24f5664fb14959becdfee1faf76355
prashant169 <prashantphad2018@gmail.com>
2021-10-14 19:19:00 -0800

    Added:Program for the check phone number is valid or not

--- UserRegistrationRegex/src/com/bridgelabz/regex/Regex.java
+++ UserRegistrationRegex/src/com/bridgelabz/regex/Regex.java
@@ -4,8 +4,9 @@ public class Regex {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the User Registration Problem using Regex!!");
 		UserOperator user = new UserOperator();
-		user.firstName("prashant");
-		user.lastName("phad");
+		user.firstName("Prashant");
+		user.lastName("Phad");
 		user.emailaddress("prashantphad2018@gmail.com");
+		user.phoneNo("91 9970899057");
 	}
 }
